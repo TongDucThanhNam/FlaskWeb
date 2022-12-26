2f93db32a557d6d494240a69552887ed8ed3f4ae
Tong Duc Thanh Nam <TeraSumi@wnlbq.onmicrosoft.com>
2022-12-26 11:25:17 +0700

    addd

--- app/main/views.py
+++ app/main/views.py
@@ -14,3 +14,62 @@ def services():
     return render_template('services.html')
 
 
+# route project
+@main.route('/projects')
+def project():
+    return render_template('projects.html')
+
+
+# route pricing
+@main.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+
+# route product
+@main.route('/product')
+def product():
+    return render_template('products.html')
+
+#route products
+@main.route('/project_init')
+def project_init():
+    return render_template('project_init.html')
+
+
+# route shopping cart
+@main.route('/shopping_cart')
+def shopping_cart():
+    return render_template('shopping-cart.html')
+
+
+# route Team
+@main.route('/team')
+def team():
+    return render_template('team.html')
+
+
+# route testimonials
+@main.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+
+# route contacts
+@main.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
+# route login
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+
+# route signup
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
