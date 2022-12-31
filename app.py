3132822d133140897ddfeb51f4208ea4aee08ce7
Tong Duc Thanh Nam <TeraSumi@wnlbq.onmicrosoft.com>
2022-12-31 17:14:06 +0700

    try app platform

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from app import create_app
 
-# app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-#
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=8080)
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
-    create_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-    create_app.run()
-else:
-    gunicorn_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    app.run(host="0.0.0.0", port=8080)
+
+# if __name__ == '__main__':
+#     create_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+#     create_app.run()
+# else:
+#     gunicorn_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
