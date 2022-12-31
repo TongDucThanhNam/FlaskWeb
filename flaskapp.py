aa5f3adcc0e29fd50cfdc24c6654f59e0db13205
Tong Duc Thanh Nam <TeraSumi@wnlbq.onmicrosoft.com>
2022-12-31 17:46:13 +0700

    try app platform

--- flaskapp.py
+++ flaskapp.py
@@ -4,10 +4,4 @@ from app import create_app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
-
-# if __name__ == '__main__':
-#     create_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-#     create_app.run()
-# else:
-#     gunicorn_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    app.run(host="0.0.0.0", port=5000)
