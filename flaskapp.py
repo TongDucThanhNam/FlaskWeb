a44958d78106fcfb6baa4caa76920177c128ca23
Tong Duc Thanh Nam <TeraSumi@wnlbq.onmicrosoft.com>
2022-12-31 10:39:51 +0700

    sửa host

--- flaskapp.py
+++ flaskapp.py
@@ -5,4 +5,4 @@ from app import create_app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='143.198.200.5', port=5000)
